aa5ff43b1719ad39ba9428432b5d890e064d12f9
claudeus123 <claudio.pizarro@alumnos.ucn.cl>
2022-10-25 14:48:54 -0300

    conexion

--- taller3.py
+++ taller3.py
@@ -1,7 +1,21 @@
 # -*- coding: utf-8 -*-
+from psycopg2 import connect, Error
 """
 Created on Mon Oct 24 20:24:04 2022
 """
+pw = 'anashe123'
+def connection():
+    try:
+        connection = connect(host = 'localhost', database = 'T3',user='postgres',password=pw,port='5432')
+        return connection
+    except (Exception, Error) as error:
+        connection.rollback()
+        print("Error: ",error)
+    
+# def insert_query(query, data):
+#     try:
+#         con = connection()
+            
 
 class Usuario:
     def __init__(self,rut,contraseña):
